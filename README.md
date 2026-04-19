28c2e21e5fb9dd83c89d81d04ad8a288e183c356
MoskiSoftware <moskisoftware@gmail.com>
2026-04-19 09:20:03 +0200

    add High Names

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+pip install uv
 pip install streamlit
 pip install plotly
 
--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,15 @@
-import streamlit as st
 import pandas as pd
 import plotly.express as px
+import streamlit as st
+from streamlit.components.v1 import html
+
+htmlText = "<div class='content--canvas' style='border-radius: 25px;'>"
+htmlText += "<div style='font-size: 30px; top:30px; font-family:verdana; color:rgb(250, 250, 250); text-align: center; position: relative; vertical-align: middle;z-index: 100;'><b>Hackaton2026 RIPCS</b></div></div>"
+
+htmlText += "<script src='https://tympanus.net/Development/AmbientCanvasBackgrounds/js/noise.min.js'></script>"
+htmlText += "<script src='https://tympanus.net/Development/AmbientCanvasBackgrounds/js/coalesce.js'></script>"
+htmlText += "<script src='https://tympanus.net/Development/AmbientCanvasBackgrounds/js/util.js'></script>"
+html(htmlText)
 
 st.set_page_config(
     page_title="Hackaton2026 RIPCS",
@@ -10,8 +19,6 @@ st.set_page_config(
 
 """
 # :material/query_stats: Hackaton2026 RIPCS
-
-GTA VI is going
 """
 
 ""  # Add some space.
@@ -20,10 +27,23 @@ DEFAULT_RULE = ["ALL", "R1", "R2", "R3", "R6", "R7", "R8", "R10", "R12", "R13", 
 outputsDf = pd.read_csv('./data/outputs.csv')
 outputAllDf = pd.read_csv('./data/output_all_rules.csv')
 
+categoryHighDf = outputsDf[outputsDf["risk_category"] == 'HIGH']
+categoryHighNewDf = pd.DataFrame({'transaction': ['Not set'], 'name': ['Not set']})
+
+for index, row in categoryHighDf.iterrows():
+    categoryHighNewDf.loc[-1] = [outputAllDf[outputAllDf["transaction_id"] == row['transaction_id']].iloc[-1].transaction_id, outputAllDf[outputAllDf["transaction_id"] == row['transaction_id']].iloc[-1].entered_beneficiary_name]
+    categoryHighNewDf.index = categoryHighNewDf.index + 1 
+
+categoryHighNewDf = categoryHighNewDf.drop_duplicates(subset=["name"])
+
 with st.sidebar:
     st.header("Settings")
     ruleType = st.selectbox("Rule type", DEFAULT_RULE, index=0)
+    categoryHigh = st.selectbox("Category High", categoryHighNewDf['name'], index=0)
 ""
+if categoryHigh != 'Not set':
+    st.title('Category High - ' + categoryHigh, text_alignment="center")
+    outputAllDf[outputAllDf["entered_beneficiary_name"] == categoryHigh][['transaction_id', 'rule_id', 'remarks', 'channel', 'amount', 'official_beneficiary_account_name']]
 
 if ruleType != 'ALL':
     outputAllDf = outputAllDf[outputAllDf["rule_id"] == ruleType]
@@ -50,5 +70,15 @@ else:
     ruleFig = px.bar(dfRules, x='rule_id', y='Count')
     st.plotly_chart(ruleFig, theme=None)
 
+    dfCustomer = outputAllDf.groupby(['entered_beneficiary_name']).size().reset_index(name='Count')
+    dfCustomer = dfCustomer.sort_values(by=["Count"])
+    dfCustomer = dfCustomer.iloc[-1]
+
+    htmlText = "<div style='font-size: 22px; position: relative; text-align: center;'><img src='https://www.api.moski.pl/gallery/cs/cs.png' style='width:400px;'><div style='position: absolute;top: 55%;left: 50%;transform: translate(-50%, -50%);'><b>" + dfCustomer['entered_beneficiary_name']
+    htmlText += "<br/>"
+    htmlText += "Rules count: "
+    htmlText += str(dfCustomer['Count'])
+    htmlText += "!</b></div></div>"
+    st.html(htmlText)
 ""
 
